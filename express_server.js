2785ba4db125580b7f4f2a88a89745072a36f2f9
kadia <kdabraeux@gmail.com>
2021-08-31 03:00:51 +0000

    urls_show file addedto display a single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -9,15 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//listens to port
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 //sends hello to client browser
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//listens to port
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 //adds a new endpoint ex localhost8080/urls.json that displays a json string
 app.get("/urls.json", (req, res) => {
@@ -32,7 +33,15 @@ app.get("/hello", (req, res) => {
 //contains an object called urlDatabase, which we use to keep track of all the URLs and their shortened forms, corresp to all urls in urls_index.ejs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-
+  
   //passes url data to template
   res.render("urls_index", templateVars);
 });
+
+/*the value in this part of the url will be available in the req.params object
+example, if the ID of the long url was b2xVn2, then the url would look like /urls/b2xVn2 in the browser. Further, the value of req.params.shortURL would be b2xVn2.*/
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
