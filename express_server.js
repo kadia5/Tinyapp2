4016ae7f752d9722e941e517ff459106f0982d14
kadia <kdabraeux@gmail.com>
2021-09-02 23:49:48 +0000

    new

--- express_server.js
+++ express_server.js
@@ -11,6 +11,39 @@ function generateRandomString () {
   return randomString;
 }
 
+//"data store" that stores and access users in app
+const users = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: '1',
+  },
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+  },
+};
+
+// const emailAlreadyTaken = function (inputEmail, users) {
+//   for (let key in users) {
+//     console.log (users[key].email);
+//     if (users[key].email == inputEmail) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+const getUserByEmail = function (inputEmail, users) {
+  for (let key in users) {
+    if (users[key].email == inputEmail) {
+      return users[key];
+    }
+  }
+  return false;
+};
+//________________________________________
+
 const express = require ('express');
 const app = express ();
 const PORT = 8080; // default port 8080
@@ -27,9 +60,7 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com',
 };
 
-
-// ___ALL GET REQUESTS START HERE___
-
+// ________ALL GET REQUESTS START HERE___________
 
 //sends hello to client browser
 app.get ('/', (req, res) => {
@@ -50,7 +81,7 @@ app.get ('/hello', (req, res) => {
 app.get ('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies['username'],
+    user: req.cookies.user_id,
   };
   //passes url data to template
   res.render ('urls_index', templateVars);
@@ -58,7 +89,8 @@ app.get ('/urls', (req, res) => {
 
 //requests basic form page that used to submit URLs to be shortened or make "new" urls from urls_new
 app.get ('/urls/new', (req, res) => {
-  const templateVars = {username: req.cookies['username']};
+  const user = users[req.cookies.user_id];
+  const templateVars = {user};
   res.render ('urls_new', templateVars);
 });
 
@@ -70,7 +102,7 @@ app.get ('/urls/:shortURL', (req, res) => {
   let shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   //longURL takes in obj urlDatabase to gain access one of the long urls
-  const templateVars = {shortURL, longURL, username: req.cookies['username']};
+  const templateVars = {shortURL, longURL, user: req.cookies.user_id};
   res.render ('urls_show', templateVars);
 });
 
@@ -80,19 +112,24 @@ app.get ('/u/:shortURL', (req, res) => {
   // if (!longURL.includes('http://')) {
   //   longURL = 'http://' + longURL;
   // }
-  res.redirect(longURL);
+  res.redirect (longURL);
 });
 
 app.get ('/login', (req, res) => {
-  let username = req.body.username;
-  const templateVars = { username: req.cookies['username']};
+  const user = req.body.user_id;
+  const templateVars = {user: req.cookies.user_id};
 
-  res.render('urls_login', templateVars);
+  res.render ('urls_login', templateVars);
 });
 
+app.get ('/register', (req, res) => {
+  let user = req.body.user_id;
+  const templateVars = {user: req.cookies.user_id};
 
-// ____ALL POST REQUESTS START HERE____
+  res.render ('urls_register', templateVars);
+});
 
+// __________ALL POST REQUESTS START HERE___________
 
 //generates a random string when redirectd to urls/shorturls
 app.post ('/urls', (req, res) => {
@@ -105,10 +142,11 @@ app.post ('/urls', (req, res) => {
   res.redirect (`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
 
-//lets you edit a posted link and redirects to edit page for shorturls
-app.post ('/urls/:shortURL/edit', (req, res) => {
+//lets you EDIT a posted link and redirects to edit page for shorturls
+app.post ('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
-  res.redirect (`/urls/${shortURL}`);
+  urlDatabase[shortURL] = req.body.url;
+  res.redirect ('/urls/');
 });
 
 //lets you delete a posted link/shorturl
@@ -118,33 +156,77 @@ app.post ('/urls/:shortURL/delete', (req, res) => {
   res.redirect ('/urls');
 });
 
-//templateVar will set and define username in all other areas.
+//templateVar will set and define user in all other areas.
 app.post ('/login', (req, res) => {
-  let username = req.body.username;
-  res.cookie ('username', username);
-  res.redirect ('/urls');
+  const user_id = generateRandomString ();
+  const user = {
+    id: user_id,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  users[user_id] = user;
+  if (req.body.user_id === '' || req.body.password === '') {
+    return res.status (403).send ('Enter email or password!');
+  }
+  if (getUserByEmail (req.body.email, users)) {
+    let user = getUserByEmail (req.body.email, users);
+    let checkPassword = user.password;
+    // console.log(user.password)
+    console.log (checkPassword);
+
+    if (!checkPassword) {
+      return res.status (403).send ("Password doesn't match");
+    }
+    res.cookie ('user_id', user_id);
+    res.redirect (`/urls/`);
+  } else {
+    return res.status (403).send ("Email or password doesn't match");
+  }
 });
 
 app.post ('/logout', (req, res) => {
-  // let username = req.body.username;
-  res.clearCookie("username");
-  res.redirect('/urls/');
+  // let user = req.body.user;
+  res.clearCookie ('user_id');
+  res.redirect ('/urls/');
 });
+
+//user stores user info and passes it into the global users object + checks if user exists before registration
+app.post ('/register', (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    return res.status (400).send ('Enter email or password!');
+  }
+  if (getUserByEmail (req.body.email, users)) {
+    return res.status (400).send ('Email Already Taken');
+  }
+  const user_id = generateRandomString ();
+  const user = {
+    id: user_id,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  users[user_id] = user;
+  res.cookie ('user_id', user_id);
+  res.redirect ('/urls');
+});
+
 //below code displays username
 // const templateVars = {
 //   username: req.cookies["username"],
 // };
 // res.render("urls_index", templateVars);
 
-
-
-
-
-
-
-
-
 //listens to port
 app.listen (PORT, () => {
   console.log (`Example app listening on port ${PORT}!`);
 });
+
+/* 
+BUGS TO FIX:
+ugly login and register layout
+ugly logout layout when logged in
+returns object object
+login bug shows password doesnt match when correct password entered
+doesnt show username email when logged in
+missing user id email in header
+new url creation leads to edit page instead of submitting straight to url list page*idk if right*
+*/
